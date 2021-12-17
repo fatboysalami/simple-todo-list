cf8517ab2c63c52cc472c34a791a7325f9b5a19c
Ruslan <ruslan.akopyan@ukr.net>
2021-12-17 17:40:17 +0200

    Add files via upload

---
+++ src/TodoItems.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import FlipMove from "react-flip-move";
+
+class TodoItems extends Component{
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    delete(key){
+        this.props.delete(key);
+    }
+
+    createTasks(item) {
+        return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+    }
+
+    render(){
+        let todoEntries = this.props.entries;
+        let listItem = todoEntries.map(this.createTasks);
+
+        return(
+            <ul className="theList">
+                <FlipMove duration={550} easing="ease-out">
+                    {listItem}
+                </FlipMove>
+            </ul>
+        )
+    }
+}
+
+export default TodoItems;
+
+
+
